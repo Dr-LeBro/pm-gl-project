8b58247e7973c75aa261cd7be07b0c4c2a10d611
Ribooki <rallemannd@gmail.com>
2019-11-12 09:25:24 +0100

    Ajout des premiers tests - commentés car la dépendance n'est pas encore dans le projet.

--- test/pacman/MainTest.java
+++ test/pacman/MainTest.java
@@ -1,9 +1,39 @@
 package pacman;
 
+import org.junit.jupiter.api.*;
+
+@DisplayName("Ma class de test de tests")
 class MainTest {
 
-    @org.junit.jupiter.api.Test
-    void main() {
-        
+    @BeforeAll
+    static void initAll() {
+        System.out.println("beforeAll");
+    }
+
+    @BeforeEach
+    void init() {
+        System.out.println("beforeEach");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("afterEach");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("afterAll");
+    }
+
+    @Test
+    void simpleTest() {
+        System.out.println("simpleTest");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("secondTest");
+        Assertions.assertTrue(true);
     }
 }
---
+++ test/pacman/engine/core/Entity/MovableEntityTest.java
@@ -0,0 +1,38 @@
+package pacman.engine.core.Entity;
+/*
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MovableEntityTest {
+
+    @ParameterizedTest
+    @EnumSource(EntityType.class)
+    void getTypeSimple(EntityType type) {
+        Entity entityTest = new MovableEntity(type);
+        Assertions.assertEquals(type, entityTest.getType());
+    }
+
+    @ParameterizedTest
+    @EnumSource(EntityType.class)
+    void getTypeCoord(EntityType type) {
+        Entity entityTest = new MovableEntity(type, 0, 0);
+        Assertions.assertEquals(type, entityTest.getType());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -1, 0, 1, 100})
+    void getX(int x) {
+        Entity entityTest = new StaticEntity(EntityType.EMPTY, x, 0);
+        Assertions.assertEquals(x, entityTest.getX());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-100, -1, 0, 1, 100})
+    void getY(int y) {
+        Entity entityTest = new StaticEntity(EntityType.EMPTY, 0, y);
+        Assertions.assertEquals(y, entityTest.getY());
+    }
+}*/
