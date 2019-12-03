d05f25156990228725023116d6a57efcd916c588
jerem <jeremie84.niel@hotmail.fr>
2019-12-03 10:30:27 +0100

    No more NullPointerException when the game is over

--- src/pacman/engine/physic/hitBox/HitBox.java
+++ src/pacman/engine/physic/hitBox/HitBox.java
@@ -4,6 +4,8 @@ import pacman.engine.core.Entity.Entity;
 
 public class HitBox {
     public boolean isInContact(double sizeX, double sizeY, double x, double y, Entity e) {
-        return (Math.abs(x - e.getX()) < (sizeX + e.getSizeX())/2.0 && Math.abs(y - e.getY()) < (sizeY + e.getSizeY())/2.0) && e.isVisible();
+        if(e != null)
+            return (Math.abs(x - e.getX()) < (sizeX + e.getSizeX())/2.0 && Math.abs(y - e.getY()) < (sizeY + e.getSizeY())/2.0) && e.isVisible();
+        return false;
     }
 }
